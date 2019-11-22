5e31bea7b78ecea0cac40d5b907d451b12d4aa80
Norquis R Suárez <nolkis@gmail.com>
2019-11-22 12:16:20 -0400

    Tarea2-3

---
+++ Tarea2-3/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tarea2_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var Coches = new List<Models.Coche>
+            {
+                new Models.Coche{Tipo = "Jeep",Marca = "BMW",Modelo ="X6",Color ="Azul" },
+                new Models.Coche{Tipo = "Auto",Marca = "Mercedes",Modelo ="A200",Color ="Blanco" },
+                new Models.Coche{Tipo = "Jeep",Marca = "BMW",Modelo ="X6",Color ="Azul" },
+                new Models.Coche{Tipo = "Auto",Marca = "Ford",Modelo ="Mustang",Color ="Rojo" },
+                };
+            return View(Coches);
+        }
+    }
+
+}
---
+++ Tarea2-3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tarea2_3.MvcApplication" Language="C#" %>
